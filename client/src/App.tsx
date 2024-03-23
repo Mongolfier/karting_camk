5cb7980227186a8caedfd3c77f1e9e2ab6f07575
Dmitry Mumrenkov <dimony4519@gmail.com>
2024-03-23 18:45:15 +0300

    Add strapi

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,16 @@ import { router } from 'shared/config/routeConfig';
 import { Header } from 'features/Header';
 import { Suspense } from 'react';
 
+async function test() {
+  await fetch('http://localhost:1337/api/news/2', {
+    headers: {
+      'Authorization': `Bearer ${process.env.REACT_APP_BEARER}`
+    }
+  })
+}
+
+test()
+
 function App() {
   return (
     // FIXME: Добавить фоллбэк лоадер?
