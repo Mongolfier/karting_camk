17b97474b263015fb7c21f5673316befc56fd573
Dmitry Mumrenkov <dimony4519@gmail.com>
2024-04-09 23:41:10 +0300

    Small styling footer for mobile

--- client/src/features/Footer/index.tsx
+++ client/src/features/Footer/index.tsx
@@ -5,12 +5,15 @@ import { ContactsServiceService } from "services/ContactsService";
 import { Nav } from "features/Nav";
 import { Text } from "shared/ui/Text";
 import { Logo } from "shared/ui/Logo";
+import { Telephone } from "shared/ui/Telephone";
 
 import cls from "./index.module.css";
 
 export const Footer = () => {
   const { data: result } = useQuery(ContactsServiceService.getContactsQuery());
   const supportEmail = result?.data[0].attributes.supportEmail;
+  const telephones = result?.data[0].attributes.telephone.split("&");
+  const address = result?.data[0].attributes.address;
 
   return (
     <section className={cls.Footer}>
@@ -19,13 +22,30 @@ export const Footer = () => {
       <div className={cls.footerWrapper}>
         <div className={cls.footerBlock}>
           <h3 className={cls.footerHeader}>Контакты</h3>
+
+          <ul className={cls.footerTelephonesList}>
+            {telephones?.map((telephone, index) => (
+              <li className={cls.contactsTelephoneItem} key={telephone}>
+                {index !== 0 && <span className={cls.redDot} />}
+                <Telephone className={cls.contactsTelephone} href={telephone}>
+                  {telephone}
+                </Telephone>
+              </li>
+            ))}
+          </ul>
+
+          <Text className={cls.address}>{address}</Text>
         </div>
         <div className={cls.footerBlock}>
           <h3 className={cls.footerHeader}>Карта сайта</h3>
           <Nav className={cls.footerNav} classNameItem={cls.footerNavItem} />
         </div>
         <div className={cn(cls.footerBlock, cls.textBlock)}>
-          <Logo containerClassName={cls.footerLogo} />
+          <Logo
+            className={cls.logoIcon}
+            containerClassName={cls.footerLogo}
+            firstLexemClassName={cls.firstLexem}
+          />
 
           <Text className={cls.subscription}>ПОУ ЦАМК ДОСААФ России, 2024</Text>
 
@@ -33,7 +53,9 @@ export const Footer = () => {
 
           <Text className={cls.support}>
             По вопросам пожеланиям и ошибкам на сайте пишите на почту&nbsp;
-            <a href={`mailto:${supportEmail}`}>{supportEmail}</a>
+            <a className={cls.link} href={`mailto:${supportEmail}`}>
+              {supportEmail}
+            </a>
           </Text>
         </div>
       </div>
--- client/src/shared/ui/Logo/index.tsx
+++ client/src/shared/ui/Logo/index.tsx
@@ -7,21 +7,20 @@ import { ReactComponent as LogoIcon } from "shared/assets/logo.svg";
 
 interface LogoProps extends React.SVGProps<SVGSVGElement> {
   containerClassName?: string;
+  firstLexemClassName?: string;
 }
 export const Logo: FC<LogoProps> = (props) => {
-  const { className, containerClassName, ...rest } = props;
+  const { className, containerClassName, firstLexemClassName, ...rest } = props;
 
   return (
     <a href={RoutePath.main} className={cn(cls.Logo__link, containerClassName)} target="_self">
       <LogoIcon
-        width="64"
-        height="64"
         className={cn(cls.Logo__icon, className)}
         {...rest}
       />
 
       <p>
-        <span className={cls.Logo__firstString}>КАРТИНГ</span>
+        <span className={cn(cls.Logo__firstString, firstLexemClassName)}>КАРТИНГ</span>
         <span>ЦАМК</span>
       </p>
     </a>
